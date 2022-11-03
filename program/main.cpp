31da2ab8af78fc1defffecd475b2cf2e7b71cdd9
student <student@it.local>
2022-11-03 12:38:39 +0100

    wer.1.0

--- program/main.cpp
+++ program/main.cpp
@@ -4,6 +4,25 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    float a,b,m_z;
+
+    do{
+    cout << "Podaj a: " << endl;
+    cin>>a;
+
+    }while(a==0);
+
+
+    cout << "Podaj b: " << endl;
+    cin>>b;
+
+
+   m_z = (-b)/a;
+
+
+cout<<"Miejsce zerowe funkcji "<<m_z<<endl;
+
+
     return 0;
 }
