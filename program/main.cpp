73b2b785e8c90623395f28708d6df6f3b676d56d
student <student@it.local>
2022-11-03 12:50:19 +0100

    wer.1.2

--- program/main.cpp
+++ program/main.cpp
@@ -6,22 +6,51 @@ int main()
 {
 
     float a,b,m_z;
-
+/*
     do{
     cout << "Podaj a: " << endl;
     cin>>a;
 
     }while(a==0);
 
+*/
+
+    cout << "Podaj a: " << endl;
+    cin>>a;
 
     cout << "Podaj b: " << endl;
     cin>>b;
 
 
-   m_z = (-b)/a;
+    if(a==0){
+
+             cout<<"Brak mikejsc zerowych"<<endl;
+
+        if(b==0){
+
+        cout<<"Jest nieskonczenie wiele miejsc zerowych"<<endl;
+
+    }
+
+    }  if(b==0){
+
+        cout<<"miejsce zerowe to 0"<<endl;
+
+    }else {
+
+     m_z = (-b)/a;
 
 
 cout<<"Miejsce zerowe funkcji "<<m_z<<endl;
+
+
+
+
+    }
+
+
+
+
 
 
     return 0;
