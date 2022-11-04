8583c0f8c16a26aeeb087e5cbff5ef74853d24f6
pawel14011 <kontaktinfo48@gmail.com>
2022-11-04 15:58:03 +0100

    w.2.3->2.4

--- program/main.cpp
+++ program/main.cpp
@@ -5,82 +5,96 @@ using namespace std;
 int main()
 {
 
-    float a,b,c,m_z1,delta,m_z2;
+    float a,b,c,m_z1,delta,m_z2,odp,m_z;
+
+
+    cout<<"Wybierz: "<<endl;
+    cout<<"1 -> Liniowa: \n2 -> Kwadratowa: "<<endl;
+    cin>>odp;
 
 
 
-    cout << "Podaj a: " << endl;
-    cin>>a;
-
-if(a==0){
-
- cout<<"jest to funkcja liniowa"<<endl;
+    if(odp == 1){ //Jezeli wybral liniowa
 
 
-    cout << "Podaj b: " << endl;
-    cin>>b;
+         cout<<"Wybrales funkcje liniowa"<<endl;
 
-    cout << "Podaj c: " << endl;
-    cin>>c;
+         cout << "Podaj a: " << endl; cin>>a;
 
-
-     m_z1 = -c/b;
-     cout<<"miejsce zerowe to "<<m_z1<<endl;
+         cout << "Podaj b: " << endl; cin>>b;
 
 
-
-}
-
-
-    cout << "Podaj b: " << endl;
-    cin>>b;
-
-    cout << "Podaj c: " << endl;
-    cin>>c;
+         m_z = (-b)/a;
 
 
+        if(a==0){
+            cout<<"Brak mikejsc zerowych"<<endl;
 
- delta = (b*b)-4*a*c;
+                if(b==0){
+                    cout<<"Jest nieskonczenie wiele miejsc zerowych"<<endl;
+
+                }
+        }
 
 
+        if(b==0){
+            cout<<"miejsce zerowe funkcji liniowej to 0"<<endl;
 
-delta = sqrt(delta);
+        }else{
+
+                m_z = (-b)/a;
+                cout<<"Miejsce zerowe funkcji liniowej"<<m_z<<endl;
+             }
 
 
 
+    }else if (odp == 2){     //jezeli wybral kwadratowa
 
- if(delta > 0 ){
+        cout<<"Wybrales funkcje kwadratowa"<<endl;
 
-    delta = sqrt(delta);
-    m_z1 = (-b-delta)/2*a;
-    m_z2 = (-b+delta)/2*a;
-
-    cout<<m_z1<<endl;
-    cout<<m_z2<<endl;
-
-
- }else if (delta == 0 ){
-
- m_z1 = -b/2*a;
-  cout<<m_z1<<endl;
-
-
- }else if(delta < 0){
-
-cout<<"Nie ma miejsc zerowych"<<endl;
-
- }else cout<<"B³¹d"<<endl;
+        cout << "Podaj a: " << endl;
+        cin>>a;
 
 
 
+        while(a==0){            //sprawdza a czy jest zerem
+            cout << "Podales 0. ";
+            cout << "Podaj a: " << endl;
+            cin>>a;
+        }
 
 
+        cout << "Podaj b: " << endl;
+        cin>>b;
+
+        cout << "Podaj c: " << endl;
+        cin>>c;
 
 
+        delta = (b*b)-4*a*c;  //liczy delte
+        delta = sqrt(delta);   //robi pierwiastek z delty
 
 
+        if(delta > 0 ){
+            delta = sqrt(delta);
+            m_z1 = (-b-delta)/2*a;
+            m_z2 = (-b+delta)/2*a;
 
+            cout<<m_z1<<endl;
+            cout<<m_z2<<endl;
 
+        }else if (delta == 0 ){
 
-    return 0;
+             m_z1 = -b/2*a;
+
+            cout<<m_z1<<endl;
+
+        }else if(delta < 0){
+
+            cout<<"Nie ma miejsc zerowych"<<endl;
+
+        }else cout<<"Blad"<<endl;
+
+    }
+ return 0;
 }
